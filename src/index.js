fb8c289c73e1b64e0cd9d5a8ec5cced57e392b87
Jason Chuang <chuangtcee@gmail.com>
2022-09-10 06:02:22 +0800

    bump up writing style to react 18

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 const DATA = [
   { id: "todo-0", name: "Eat", completed: true },
@@ -10,9 +9,9 @@ const DATA = [
   { id: "todo-2", name: "Repeat", completed: false }
 ];
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App tasks={DATA} />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App tasks={DATA}  />
+  </React.StrictMode>
 );
